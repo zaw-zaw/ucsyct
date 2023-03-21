cb2770349cde02bf1529d0e908d3e054cf7add81
zaw-zaw <zawzawlatt169@gmail.com>
2023-03-21 15:07:19 +0800

    Add files via upload

--- AZVM01.tf
+++ AZVM01.tf
@@ -1,5 +1,46 @@
+#Creating Virtual Network and Subnet for VM01
+resource "azurerm_virtual_network" "VNET01" {
+  name                = "ZZL-VNET01"
+  resource_group_name = azurerm_resource_group.RG01.name
+  location            = azurerm_resource_group.RG01.location
+  address_space       = ["172.16.0.0/16"]
+}
+resource "azurerm_subnet" "Subnet01" {
+  name                 = "ZZL-Subnet01"
+  resource_group_name  = azurerm_resource_group.RG01.name
+  virtual_network_name = azurerm_virtual_network.VNET01.name
+  address_prefixes     = ["172.16.10.0/24"]
+}
+
+#Creating Public ip 
+resource "azurerm_public_ip" "PublicIP01" {
+  name                = "ZZL-PublicIP01"
+  location            = azurerm_resource_group.RG01.location
+  resource_group_name = azurerm_resource_group.RG01.name
+
+  allocation_method = "Dynamic"
+}
+
+#Network Security Group
+resource "azurerm_network_security_group" "NSG01" {
+  name = "ZZL-NSG01"
+  location = azurerm_resource_group.RG01.location
+  resource_group_name = azurerm_resource_group.RG01.name
+  
+  security_rule  {
+    access = "Allow"
+    direction = "Inbound"
+    name = "allow-ssh"
+    priority = 100
+    protocol = "Tcp"
+    source_address_prefix = "*"
+    destination_port_range     = "22"
+    source_port_range = "*"
+    destination_address_prefix = "*"
+  } 
+}
 #Creating Network INT for Private and Public Subnet
-resource "azurerm_network_interface" "VMNIC" {
+resource "azurerm_network_interface" "VMNIC01" {
   name                = "VMNIC01"
   location            = azurerm_resource_group.RG01.location
   resource_group_name = azurerm_resource_group.RG01.name
@@ -8,13 +49,13 @@ resource "azurerm_network_interface" "VMNIC" {
     name                          = "internal"
     subnet_id                     = azurerm_subnet.Subnet01.id
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id = azurerm_public_ip.PublicIP.id
+    public_ip_address_id = azurerm_public_ip.PublicIP01.id
   }
 }
 
 #Associating VMNIC and NSG
-resource "azurerm_network_interface_security_group_association" "Assocate" {
-  network_interface_id = azurerm_network_interface.VMNIC.id
+resource "azurerm_network_interface_security_group_association" "Assocate01" {
+  network_interface_id = azurerm_network_interface.VMNIC01.id
   network_security_group_id = azurerm_network_security_group.NSG01.id
 }
 
@@ -26,7 +67,7 @@ resource "azurerm_linux_virtual_machine" "TestVM01" {
   size                = "Standard_B1s"
   admin_username      = "zzl"
   network_interface_ids = [
-    azurerm_network_interface.VMNIC.id,
+    azurerm_network_interface.VMNIC01.id,
   ]
 
   admin_ssh_key {
